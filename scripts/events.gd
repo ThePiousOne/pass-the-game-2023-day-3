a92f4caa38e51ce1f0e4616cf353fe844731b887
Gary Josack <gary@byoteki.com>
2023-07-25 23:48:52 -0700

    add pause menu

--- scripts/events.gd
+++ scripts/events.gd
@@ -5,6 +5,12 @@ enum SCENE {
     LEVEL,
 }
 
+enum MENU {
+    PAUSE,
+    SETTINGS,
+}
+
 signal toggle_fullscreen(fullscreen: bool)
+signal menu_summoned(menu: MENU)
 signal menu_dismissed(menu)
 signal change_scene(scene: SCENE)
